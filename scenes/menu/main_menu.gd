6ab8c79ed663c577348c01adcbce7b7c9183b2f4
Ronaldo Gomes <rongomes15@gmail.com>
2026-08-20 16:18:09 +1000

    deploying web

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -1,8 +1,14 @@
 extends Control
 
+@onready var quit_button = $VBoxContainer/QuitButton
+
+func _ready() -> void:
+	var platform = OS.get_name()
+	if platform == "Web":
+		quit_button.hide()
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
